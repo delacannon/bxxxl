3312778a8e2dc81a6f9512a9d734f107f50be4c1
delacannon <delacannon@gmail.com>
2018-07-19 15:36:42 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 import _ from 'lodash'
-
 import { addGrid, updateGrid, spriteData } from "./actions";
 import { connect } from "react-redux"
-
 import Tiles from './components/Tiles'
-
-const grid_items= ['', '', '', '','', '', '', ''];
-
-var grid =  _.map(grid_items, (title, index) => {
-  return {id: index, title: title, grids: _.map(_.range(8), index => {
-    return {id: index, active: false};
-  })}
-});
-
+import {Container, Row, Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import Konva from 'konva';
 
 class App extends Component {
 
@@ -22,10 +14,21 @@ class App extends Component {
     this.state = {
       isDown: false
     }
+
+    let number = 8
+    this.grid_items = _.range(number)
+
+    this.grid =  _.map(this.grid_items, (title, index) => {
+      return {id: index, grids: _.map(_.range(number), index => {
+        return {id: index, active: false};
+      })}
+    });
+
   }
 
-  onMouseDown(rowId, colId) {
+  onMouseDown(rowId, colId,e) {
 
+if(e.nativeEvent.which === 1 || e.type==='click'){
     if(!this.state.isDown){
       let isActive = !this.props.grid[rowId].grids[colId].active ? true : false
       this.props.updateGrid(rowId, colId, isActive )
@@ -33,6 +36,7 @@ class App extends Component {
         isDown:true
       })
     }
+  }
 
   }
 
@@ -44,17 +48,23 @@ class App extends Component {
 
   }
 
-  onMouseMove(rowId, colId){
-    //let isActive = !this.props.grid[rowId].grids[colId].active ? true : false
+  onMouseMove(rowId, colId,e){
+    
+    let posx = Math.floor(e.clientX/30)
+    let posy = Math.floor(e.clientY/30)
+    let isActive = !this.props.grid[rowId].grids[colId].active ? true : false
+
     if(this.state.isDown){
+      //let isActive = !this.props.grid[rowId].grids[colId].active ? true : false
       this.props.updateGrid(rowId, colId, true )
+
     }
   
   }
 
   componentDidMount(){
 
-   this.props.addGrid(grid)
+   this.props.addGrid(this.grid)
 
   }
 
@@ -78,8 +88,44 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.props.grid!=null && <Tiles onMouseUp={this.onMouseUp.bind(this)} onMouseMove={this.onMouseMove.bind(this)} onMouseDown={this.onMouseDown.bind(this)} />}
-        {this.props.sprite_data}
+      <Container>
+        {this.props.grid!=null && 
+        <Form>
+         <FormGroup>
+          <Row>
+          <Col xs="6" sm="4">
+            <Label for="tileName">Tile Name</Label>
+            <Input type="text" name="tileName" id="tileName" placeholder="Enter tile name" />
+          </Col>
+          <Col xs="6" sm="4">
+            <Label for="tileName">Author Name</Label>
+            <Input type="text" name="tileName" id="tileName" placeholder="Enter tile name" />
+          </Col>
+          <Col xs="6" sm="4">
+            <Label for="tileName">Tags</Label>
+            <Input type="text" name="tileName" id="tileName" placeholder="Enter tile name" />
+          </Col>
+          </Row>
+        </FormGroup>
+        
+          <Row>
+            <Col xs="6" sm="4"><Tiles onMouseUp={this.onMouseUp.bind(this)} onMouseMove={this.onMouseMove.bind(this)} onMouseDown={this.onMouseDown.bind(this)} />
+            </Col>
+            <Col xs="6" sm="4">
+             <p>{/*this.props.sprite_data*/}</p>
+            </Col>
+            <Col xs="6" sm="4">
+               <Stage width={8} height={8} scaleX={4} scaleY={4}>
+                <Layer>
+                    <Rect fill={'black'} x={0} y={0} width={1} height={1}/>
+                </Layer>
+               </Stage>
+            </Col>
+          </Row>
+        </Form>
+      }
+        
+        </Container>
      </div>
     );
   }
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class Cell extends Component {
  
    render(){
-      console.log(this.props)
+ 
       var divStyle = null,
           classes = this.props.active? 'checked grid' : 'grid';
       
